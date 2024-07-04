d130378102543e49420bd03b1a7966be7804c806
Kyoo16 <mhmmdzaki1601@gmail.com>
2024-07-04 14:43:21 +0800

    kalselgo xml

---
+++ app/src/main/java/com/example/kalselgo/Info1Activity.java
@@ -0,0 +1,39 @@
+package com.example.kalselgo;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class Info1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_info1)
+    }
+
+    RatingBar ratingBar = findViewById(R.id.rb_ratingBar);
+ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            if (fromUser) {
+                // Do something with the rating
+                Toast.makeText(getApplicationContext(), "Rating: " + rating, Toast.LENGTH_SHORT).show();
+            }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//
+
+}
